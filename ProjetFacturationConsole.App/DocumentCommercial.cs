d7b54c499594afe39e39dd48a4510c1dedbe15ab
Olutobi <olutobi.akinsinmide@edouardgand.fr>
2026-04-26 15:57:42 +0200

    Logique de la facture terminée

---
+++ ProjetFacturationConsole.App/DocumentCommercial.cs
@@ -0,0 +1,37 @@
+public abstract class DocumentCommercial {
+    protected string numero;
+    protected DateTime dateEmission;
+    protected Client client;
+    protected Entreprise entreprise;
+    protected List<LigneFacture> lignes = new List<LigneFacture>();
+
+    public string Numero { get { return numero; } set { numero = value; } }
+    public DateTime DateEmission { get { return dateEmission; } set { dateEmission = value; } }
+    public Client Client { get { return client; } set { client = value; } }
+    public Entreprise Entreprise { get { return entreprise; } set { entreprise = value; } }
+    public List<LigneFacture> Lignes { get { return lignes; } }
+
+    public void AjouterLigne(LigneFacture ligne) {
+        lignes.Add(ligne);
+    }
+
+    public decimal CalculerTotalHT() {
+        decimal total = 0;
+        foreach (var l in lignes) { total += l.CalculerTotalHT(); } //using vars to let the smart compiler choose :)
+        return total;
+    }
+
+    public decimal CalculerTotalTVA() {
+        decimal total = 0;
+        foreach (var l in lignes) { total += l.CalculerMontantTVA(); }
+        return total;
+    }
+
+    public decimal CalculerTotalTTC() {
+        decimal total = 0;
+        foreach (var l in lignes) { total += l.CalculerTotalTTC(); }
+        return total;
+    }
+
+    public abstract void AfficherFacture();
+}
---
+++ ProjetFacturationConsole.App/Facture.cs
@@ -0,0 +1,33 @@
+using System.Text;
+
+namespace ProjetFacturationConsole.App;
+
+public class Facture : DocumentCommercial {
+    public DateTime DateEcheance { get; set; }
+    public string Statut { get; set; }
+
+    public override void AfficherFacture() {
+        Console.WriteLine(ConstruireTexte());
+    }
+
+    public string ConstruireTexte() {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("FACTURE");
+        sb.AppendLine("Numéro: " + numero);
+        sb.AppendLine("Date d'émission : " + dateEmission.ToString("dd/MM/yyyy"));
+        sb.AppendLine("Date d'échéance: " + DateEcheance.ToString("dd/MM/yyyy"));
+        sb.AppendLine("Statut: " + Statut);
+        sb.AppendLine("Entreprise :");
+        sb.AppendLine(Entreprise.Id + " - " + Entreprise.Nom + " - " + Entreprise.Email);
+        sb.AppendLine("Client :");
+        sb.AppendLine(Client.Id + " - " + Client.Nom + " - " + Client.Email);
+        sb.AppendLine("Lignes :");
+        int i = 1;
+        foreach (var l in lignes) {
+            sb.AppendLine(i + ". " + l.Description + " - Qté: " + l.Quantite + " PUHT: " + l.PrixUnitaireHT + " Total TTC: " + l.CalculerTotalTTC());
+            i++;
+        }
+        sb.AppendLine("Total TTC: " + CalculerTotalTTC());
+        return sb.ToString();
+    }
+}
